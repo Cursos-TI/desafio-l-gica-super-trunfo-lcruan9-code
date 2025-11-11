ea1302a2d1afebb912db1a7ca001979d844314c5
lcruan9-code <lcruan9@gmail.com>
2025-11-11 14:51:36 +0000

    Finalização do Jogo

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -1,7 +1,7 @@
 
 #include <stdio.h>
-//#include <stdlib.h>
-//#include <string.h>
+#include <stdlib.h>
+#include <string.h>
 
 // 📇 Estrutura (struct) para representar uma Carta de Cidade
 typedef struct {
